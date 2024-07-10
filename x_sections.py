a9dbad5e3f27495cd4a8ea3eb39817971583a6e0
egor <privetegor@gmail.com>
2024-07-11 00:52:46 +0800

    1. Init commit_1

--- x_sections.py
+++ x_sections.py
@@ -3,17 +3,4 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from fpdf import FPDF
 
-
-# Function to export plots to PDF
-def export_to_pdf(df, id_lines):
-    pdf = FPDF()
-    for id_line in id_lines:
-        plot = plot_cross_section(df, id_line)
-        plot.savefig(f'temp_plot_{id_line}.png')
-        pdf.add_page()
-        pdf.image(f'temp_plot_{id_line}.png', x=10, y=10, w=190)
-        plot.close()
-    pdf.output('cross_sections.pdf')
-
-
 st.title('Cross Section Visualizer')
